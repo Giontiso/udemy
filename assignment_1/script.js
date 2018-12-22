ce153dbc4d1394c1ddafc6b6fb97f197dcfcbd0d
Egor <Hamelion74@gmail.com>
2018-12-22 13:57:51 +0300

    fixed assigment_1

--- assignment_1/script.js
+++ assignment_1/script.js
@@ -1,23 +1,24 @@
 'use strict';
 
-let money = prompt('Ваш бюджет на месяц?'),
-    time = prompt('Введите дату в формате YYYY-MM-DD'),
-    expenses = prompt('Введите обязательную статью расходов в этом месяце'),
-    price = prompt('Во сколько обойдется?'),
-    dayMon = 30;
+let money = prompt('Ваш бюджет на месяц?', ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', ''),
+    expen = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    price = prompt('Во сколько обойдется?', ''),
+    expen_2 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    price_2 = prompt('Во сколько обойдется?', '');
 
-var appData = {
+let appData = {
     budget: money,
     timeDate: time,
-    expenses: {
-        mandatoryExpenses: expenses,
-        price: price
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 
 };
 
+appData.expenses.expen = price;
+appData.expenses.expen_2 = price_2;
+
 // console.log(appData);
-console.log14(Math.floor(money/dayMon));
+console.log(appData.budget / 30);
